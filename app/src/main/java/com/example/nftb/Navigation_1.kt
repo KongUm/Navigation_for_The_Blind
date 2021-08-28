262d3b9c12bc6017007c546d722a49528db48544
KongUm <gkswngus6@gmail.com>
2021-08-29 01:08:56 +0900

    Gps 기능 구현

--- app/src/main/java/com/example/nftb/Navigation_1.kt
+++ app/src/main/java/com/example/nftb/Navigation_1.kt
@@ -2,10 +2,15 @@ package com.example.nftb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Navigation_1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation1)
+
+        val tv_current_loca = findViewById<TextView>(R.id.tv_currloca)
+        tv_current_loca.setText("$lat, $long")
+
     }
 }
--- app/src/main/java/com/example/nftb/Navigation_2.kt
+++ app/src/main/java/com/example/nftb/Navigation_2.kt
@@ -7,5 +7,7 @@ class Navigation_2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation2)
+
+
     }
 }
