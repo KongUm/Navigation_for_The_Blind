3e4cd56496f54972cbfd94855b70f8593566a1ce
KongUm <gkswngus6@gmail.com>
2021-08-30 00:02:31 +0900

    STT 기능 구현

--- app/src/main/java/com/example/nftb/Navigation_1.kt
+++ app/src/main/java/com/example/nftb/Navigation_1.kt
@@ -1,10 +1,31 @@
 package com.example.nftb
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+var address : String? = null
+
+
 
 class Navigation_1 : AppCompatActivity() {
+
+    private lateinit var speechRecognizer: SpeechRecognizer
+    private lateinit var recognitionListener: RecognitionListener
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation1)
@@ -12,5 +33,108 @@ class Navigation_1 : AppCompatActivity() {
         val tv_current_loca = findViewById<TextView>(R.id.tv_currloca)
         tv_current_loca.setText("$lat, $long")
 
+
+        requestPermission()
+
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
+
+        setListener()
+
+        val btn_stt_start : Button = findViewById(R.id.btn_sttstart)
+
+        btn_stt_start.setOnClickListener {
+            speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+            speechRecognizer.setRecognitionListener(recognitionListener)
+            speechRecognizer.startListening(intent)
+        }
+
+
     }
+
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            // 거부해도 계속 노출됨. ("다시 묻지 않음" 체크 시 노출 안됨.)
+            // 허용은 한 번만 승인되면 그 다음부터 자동으로 허용됨.
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.RECORD_AUDIO), 0)
+        }
+    }
+
+
+    private fun setListener() {
+        recognitionListener = object: RecognitionListener {
+
+            override fun onReadyForSpeech(params: Bundle?) {
+                Toast.makeText(applicationContext, "음성인식을 시작합니다.", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onBeginningOfSpeech() {
+
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+
+            }
+
+            override fun onBufferReceived(buffer: ByteArray?) {
+
+            }
+
+            override fun onEndOfSpeech() {
+
+            }
+
+            override fun onError(error: Int) {
+                val message: String
+
+                when (error) {
+                    SpeechRecognizer.ERROR_AUDIO ->
+                        message = "오디오 에러"
+                    SpeechRecognizer.ERROR_CLIENT ->
+                        message = "클라이언트 에러"
+                    SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS ->
+                        message = "퍼미션 없음"
+                    SpeechRecognizer.ERROR_NETWORK ->
+                        message = "네트워크 에러"
+                    SpeechRecognizer.ERROR_NETWORK_TIMEOUT ->
+                        message = "네트워크 타임아웃"
+                    SpeechRecognizer.ERROR_NO_MATCH ->
+                        message = "찾을 수 없음"
+                    SpeechRecognizer.ERROR_RECOGNIZER_BUSY ->
+                        message = "RECOGNIZER가 바쁨"
+                    SpeechRecognizer.ERROR_SERVER ->
+                        message = "서버가 이상함"
+                    SpeechRecognizer.ERROR_SPEECH_TIMEOUT ->
+                        message = "말하는 시간초과"
+                    else ->
+                        message = "알 수 없는 오류"
+                }
+                Toast.makeText(applicationContext, "에러 발생 $message", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResults(results: Bundle?) {
+                val matches: ArrayList<String> = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
+
+                for (i in 0 until matches.size) {
+                    address = matches[i]
+                }
+
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {
+
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle?) {
+
+            }
+
+        }
+    }
+
 }
